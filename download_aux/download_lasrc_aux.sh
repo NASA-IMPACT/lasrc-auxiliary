beb6509afea56948dbb66b6569fe445f7cb3919f
Aimee Barciauskas <aimee@developmentseed.org>
2019-11-02 00:37:15 -0400

    Add CMGDEM.hdf and compile all downloads into one script

--- download_aux/download_lasrc_aux.sh
+++ download_aux/download_lasrc_aux.sh
@@ -2,3 +2,9 @@
 cd /var/lasrc_aux/
 wget --no-http-keep-alive http://edclpdsftp.cr.usgs.gov/downloads/auxiliaries/lasrc_auxiliary/lasrc_aux.2013-2017.tar.gz
 tar -xvzf lasrc_auxiliary.2013-2017.tar.gz
+
+wget --no-http-keep-alive http://edclpdsftp.cr.usgs.gov/downloads/auxiliaries/lasrc_auxiliary/MSILUT.tar.gz
+tar -xvzf MSILUT.tar.gz
+
+wget --no-http-keep-alive http://edclpdsftp.cr.usgs.gov/downloads/auxiliaries/lasrc_auxiliary/L8/CMGDEM.hdf
+
--- download_aux/download_msilut.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /var/lasrc_aux/
-wget --no-http-keep-alive http://edclpdsftp.cr.usgs.gov/downloads/auxiliaries/lasrc_auxiliary/MSILUT.tar.gz
-tar -xvzf MSILUT.tar.gz
